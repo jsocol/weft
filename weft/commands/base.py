ae79bbd3aa191301a6a1d002c3c9a7810d63e269
James Socol <me@jamessocol.com>
2013-08-30 21:39:54 +0000

    Create a BaseCommand.

---
+++ weft/commands/base.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def make_option(*args, **kwargs):
+    return (args, kwargs)
+
+
+class BaseCommand(object):
+    command = None
+    command_options = ()
+    help = ''
+
+    @classmethod
+    def get_arguments(cls, subparsers):
+        if cls.command is None:
+            _, _, cls.command = cls.__module__.rpartition('.')
+        parser = subparsers.add_parser(cls.command, help=cls.help)
+        for (args, kwargs) in cls.command_options:
+            parser.add_argument(*args, **kwargs)
+
+    def handle(self, *args, **options):
+        """Subclasses must implement this method."""
+        raise NotImplementedError
